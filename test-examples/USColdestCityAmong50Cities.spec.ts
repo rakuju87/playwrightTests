0cb4952e5c20c31365861f40096e121376806860
Raj <raj.nises.sushil.kujur@au.pwc.com>
2023-08-21 10:40:55 +1000

    Updated the test example

--- test-examples/USColdestCityAmong50Cities.spec.ts
+++ test-examples/USColdestCityAmong50Cities.spec.ts
@@ -1,8 +1,10 @@
 import { test, expect, APIRequestContext } from '@playwright/test';
 import { ApiActions } from '../actions/apiActions';
 import { CurrentWeatherResponse } from '../models/response/currentWeather';
+import { Helper } from '../support/helper';
 let request: APIRequestContext;
 let apiActions: ApiActions;
+let helper: Helper;
 let currentWeatherResponse: CurrentWeatherResponse[];
 test.describe.serial(`WeatherBitAPI - coldest US capital city`, async () => {
     test.beforeAll(async ({ browser }) => {
@@ -11,14 +13,14 @@ test.describe.serial(`WeatherBitAPI - coldest US capital city`, async () => {
     });
     test(`Get current weather data for 50 US cities`, async () => {
         currentWeatherResponse = [];
-        const usCities = await apiActions.resize(await apiActions.getUSCities(), 50);
+        const usCities = await helper.resize(await helper.getUSCities(), 50);
         await Promise.all(usCities.map(async (city) => {
             currentWeatherResponse.push(await apiActions.currentWeatherDataByCity(city, 'US'));
         }));
     });
     test(`Verify the coldest city`, async () => {
         let coldestCity: { city: string; temp: number; };
-        coldestCity = await apiActions.getColdestCity(currentWeatherResponse);
+        coldestCity = await helper.getColdestCity(currentWeatherResponse);
         console.log(`Coldest US capital city - ${coldestCity.city} with temp - ${coldestCity.temp}`);
     });
 });
